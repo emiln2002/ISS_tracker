bc5618fa82847c1b73345d741c3760272d14bd14
niko9083 <niko9083@edu.aarhustech.dk>
2022-02-03 15:31:43 +0100

    Shiz

--- WeatherService.py
+++ WeatherService.py
@@ -1,5 +1,6 @@
-import pyowm
+from datetime import date
 from datetime import datetime
+import pyowm
 
 owm = pyowm.OWM("fc1d78e5bdb6e3e613600e91faa22e79")
 mgr = owm.weather_manager()
@@ -13,17 +14,30 @@ lat = copenhagen.lat
 one_call = mgr.one_call(lat=lat, lon=lon)
 
 now = datetime.now()
+current_minute = int(now.strftime("%M"))
 current_hour = int(now.strftime("%H"))
+current_day = int(str(date.today()).split("-")[2])
+current_month = int(str(date.today()).split("-")[1])
+current_year = int(str(date.today()).split("-")[0])
 
 
-def hourlyclouds(hour):
-    for i in range(48):
-        print(f"{hour}: {one_call.forecast_hourly[i].clouds}% cloud cover")
+class WeatherService:
+    def __init__(self, latitude, longitude):
+        self.minute = current_minute
+        self.hour = current_hour
+        self.day = current_day
+        self.month = current_month
+        self.year = current_month
 
-        hour += 1
+    def hourlyclouds(self):
+        for i in range(48):
+            print(f"{self.hour}: {one_call.forecast_hourly[i].clouds}% cloud cover")
 
-        if hour >= 24:
-            hour = 0
+            self.hour += 1
+
+            if self.hour >= 24:
+                self.hour = 0
 
 
-hourlyclouds(current_hour)
+cph = WeatherService(lat, lon)
+cph.minutelyclouds()
