8da4ef68f7d32bc5e7cea79834fda0eb121a49e8
niko9083 <niko9083@edu.aarhustech.dk>
2022-02-04 13:25:32 +0100

    WeatherService.py kan nu se, hvor skyet det er.

--- WeatherService.py
+++ WeatherService.py
@@ -6,38 +6,44 @@ owm = pyowm.OWM("fc1d78e5bdb6e3e613600e91faa22e79")
 mgr = owm.weather_manager()
 
 reg = owm.city_id_registry()
-list_of_locations = reg.locations_for('copenhagen', country='DK')
-copenhagen = list_of_locations[0]
-lon = copenhagen.lon
-lat = copenhagen.lat
+#  list_of_locations = reg.locations_for('copenhagen', country='DK')
+#  copenhagen = list_of_locations[0]
+#  lon = copenhagen.lon
+#  lat = copenhagen.lat
+lon = 10.212030
+lat = 56.158150
 
 one_call = mgr.one_call(lat=lat, lon=lon)
 
 now = datetime.now()
-current_minute = int(now.strftime("%M"))
 current_hour = int(now.strftime("%H"))
-current_day = int(str(date.today()).split("-")[2])
-current_month = int(str(date.today()).split("-")[1])
-current_year = int(str(date.today()).split("-")[0])
+cloudiness = 0
+visible = False
 
 
 class WeatherService:
     def __init__(self, latitude, longitude):
-        self.minute = current_minute
-        self.hour = current_hour
-        self.day = current_day
-        self.month = current_month
-        self.year = current_month
+        self.cloudiness = cloudiness
+        self.visible = visible
 
-    def hourlyclouds(self):
-        for i in range(48):
-            print(f"{self.hour}: {one_call.forecast_hourly[i].clouds}% cloud cover")
+    def hourlyclouds(self, hours):
+        self.cloudiness = int(one_call.forecast_hourly[hours].clouds)
 
-            self.hour += 1
+        if self.cloudiness < 25:
+            self.visible = True
 
-            if self.hour >= 24:
-                self.hour = 0
+        return self.visible
+
+    def dailyclouds(self, days):
+        self.cloudiness = int(one_call.forecast_daily[days].clouds)
+
+        if self.cloudiness < 25:
+            self.visible = True
+
+        return self.visible
 
 
-cph = WeatherService(lat, lon)
-cph.minutelyclouds()
+aarhus = WeatherService(lat, lon)
+
+print(aarhus.hourlyclouds(4))
+print(aarhus.dailyclouds(4))
