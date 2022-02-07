c0196ca110ded3a1bfb19ecec2f6f977720c60b8
niko9083 <niko9083@edu.aarhustech.dk>
2022-02-07 10:09:10 +0100

    Tilføjet unix input.

--- WeatherService.py
+++ WeatherService.py
@@ -5,45 +5,34 @@ import pyowm
 owm = pyowm.OWM("fc1d78e5bdb6e3e613600e91faa22e79")
 mgr = owm.weather_manager()
 
-reg = owm.city_id_registry()
-#  list_of_locations = reg.locations_for('copenhagen', country='DK')
-#  copenhagen = list_of_locations[0]
-#  lon = copenhagen.lon
-#  lat = copenhagen.lat
 lon = 10.212030
 lat = 56.158150
 
-one_call = mgr.one_call(lat=lat, lon=lon)
-
-now = datetime.now()
-current_hour = int(now.strftime("%H"))
-cloudiness = 0
-visible = False
-
 
 class WeatherService:
-    def __init__(self, latitude, longitude):
-        self.cloudiness = cloudiness
-        self.visible = visible
+    def __init__(self, latitude, longitude, unix):
+        self.one_call = mgr.one_call(lat=latitude, lon=longitude)
+        self.cloudiness = 0
+        self.visible = False
+        self.hours = int(unix / 3600)
+        self.days = int(unix / 86400)
 
-    def hourlyclouds(self, hours):
-        self.cloudiness = int(one_call.forecast_hourly[hours].clouds)
+    def getcloudiness(self):
+        if 0 <= self.hours <= 47:
+            self.cloudiness = int(self.one_call.forecast_hourly[self.hours].clouds)
+
+        elif self.days <= 6:
+            self.cloudiness = int(self.one_call.forecast_daily[self.days].clouds)
+
+        else:
+            return "Error: Unable to find weather info that far into the future."
 
         if self.cloudiness < 25:
             self.visible = True
 
         return self.visible
 
-    def dailyclouds(self, days):
-        self.cloudiness = int(one_call.forecast_daily[days].clouds)
 
-        if self.cloudiness < 25:
-            self.visible = True
+aarhus = WeatherService(lat, lon, 3235553)
 
-        return self.visible
-
-
-aarhus = WeatherService(lat, lon)
-
-print(aarhus.hourlyclouds(4))
-print(aarhus.dailyclouds(4))
+print(aarhus.getcloudiness())
